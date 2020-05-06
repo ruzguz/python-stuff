3109eb7ecdfa47e868a3b2c1d3b73326c1ec4c2d
Rusben Guzman <rusbenguzman@gmail.com>
2020-05-06 15:27:18 -0400

    first commit

---
+++ square.py
@@ -0,0 +1,15 @@
+import turtle
+
+window = turtle.Screen()
+
+pencil = turtle.Turtle()
+
+pencil.forward(100)
+pencil.left(90)
+pencil.forward(100)
+pencil.left(90)
+pencil.forward(100)
+pencil.left(90)
+pencil.forward(100)
+
+turtle.mainloop()
