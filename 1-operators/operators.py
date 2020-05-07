281910f8cde594293ac5caa9dc02136a131569bc
Rusben Guzman <rusbenguzman@gmail.com>
2020-05-06 20:42:32 -0400

    move files

---
+++ 1-operators/operators.py
@@ -0,0 +1,35 @@
+# Trying basics operators 
+
+# Addition
+print(1 + 1) 
+
+# Subtraction
+print(2 - 1)
+
+# Multiplication
+print(3 * 3)
+
+# Division
+print(9 / 2)
+
+# Modulus 
+print(9 % 2)
+
+# Exponentiation
+print(3 ** 3)
+
+# Floor division
+print(9 // 2)
+
+# Greater than
+print(1 > 2)
+
+# Less than 
+print(1 < 2)
+
+# Equal
+print(1 == 1)
+
+# Not equal
+print(1 != 1)
+
