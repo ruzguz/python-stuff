72ca7153b782927cae8ca1c2c3058941d3499cbb
Rusben Guzman <rusbenguzman@gmail.com>
2020-05-07 14:22:57 -0400

    print different data types

---
+++ 2-datatypes/datatypes.py
@@ -0,0 +1,20 @@
+# datatypes in python
+
+# Int 
+print("Int:")
+print(type(1))
+
+# Float 
+print("Float:")
+print(type(1.0))
+
+# String 
+print("String:")
+print(type("String"))
+
+# Bool 
+print("Bool:")
+print(type(1==1))
+
+
+
